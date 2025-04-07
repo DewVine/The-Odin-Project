a8266dec7af54ec9ee652e1d396c83d2711f5c99
DewVine <142268342+DewVine@users.noreply.github.com>
2025-04-07 09:42:31 -0400

    feat: add exercises up to palindrome and dom manipulation

--- javascript-practice/js/javascript-exercises/09_palindromes/palindromes.js
+++ javascript-practice/js/javascript-exercises/09_palindromes/palindromes.js
@@ -1,9 +1,13 @@
 const palindromes = function (string) {
-    for(let i=0; i<(Math.floor(string.length)/2); i++) {
-        if (string[i] !== string[string.length-1-i]) return false;
+    let newString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+    for(let i=0; i<(Math.floor(newString.length/2)); i++) {
+        if (newString[i] !== newString[newString.length-1-i]) return false;
     }
     return true;
 };
+
+console.log(palindromes("Animal loots foliated detail of stool lamina"));
 
 // Do not edit below this line
 module.exports = palindromes;
