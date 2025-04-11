2d837c4558de4fa529a78bb6e8667de2838584ef
DewVine <142268342+DewVine@users.noreply.github.com>
2025-04-10 21:21:59 -0400

    feat: add exercises to DOM-Manipulation practice file

--- javascript-practice/js/dom-manipulation/index.js
+++ javascript-practice/js/dom-manipulation/index.js
@@ -38,3 +38,53 @@ button.addEventListener("click", function (e) {
         e.target.style.background = "red";
     }
 });
+
+
+
+
+// mdn first active learning example
+const link = document.querySelector(".mdn-link");
+link.textContent = "Mozilla Developer Network";
+link.href = "https://developer.mozilla.org";
+
+const sect1 = document.querySelector(".mdn-active-learning-1");
+
+const para = document.createElement('p');
+para.textContent = "We hope you enjoyed the ride.";
+para.setAttribute("class", "highlight");
+sect1.appendChild(para);
+
+const text3 = document.createTextNode(
+    " - the premier source for web development knowledge."
+);
+const linkPara = document.querySelector(".para");
+linkPara.appendChild(text3);
+
+
+
+// mdn second active learning
+const list = document.querySelector(".shopping-items");
+let input = document.querySelector("#item");
+const button1 = document.querySelector(".add-item");
+
+button1.addEventListener("click", function (e) {
+    let value = input.value;
+    input.value = '';
+
+    let listItem = document.createElement("li");
+    let listSpan = document.createElement("span");
+    let listButton = document.createElement("button");
+
+    listButton.addEventListener("click", function (e) {
+        listItem.remove();
+    });
+
+    listSpan.textContent = value;
+    listButton.textContent = "Delete";
+
+    listItem.appendChild(listSpan);
+    listItem.appendChild(listButton);
+    list.appendChild(listItem);
+
+    input.focus();
+});
