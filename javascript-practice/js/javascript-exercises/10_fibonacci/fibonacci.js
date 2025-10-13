0b81a3004cbe3d383bc4484937de55642a1a40a5
DewVine <bargi@The-Second-Brain.localdomain>
2025-10-13 15:49:52 -0400

    feat(js): complete three TOP js object exercises

--- javascript-practice/js/javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-practice/js/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num<0) return "OOPS";
+    let fib = [1, 1];
+    for (let i=0; i<num; i++) {if (i>1) fib[i] = fib[i-1] + fib[i-2];}
+    if (num===0 || (Number(num)===0)) return 0;
+    else if ((typeof fib[num-1]) === "number") return (fib[num-1]);
+    return num;
 };
 
 // Do not edit below this line
---
+++ javascript-practice/js/objects-practice/objects-practice.js
@@ -0,0 +1,99 @@
+/*
+let user = {
+    name: "John",
+    age: 30,
+    greet: function() {
+        return `Hello, my name is ${this.name}!`;
+    }
+};
+
+let key = prompt("Hello, Good Sir!")
+alert("You said " + key + " to " + user.name + ".");
+
+let hello = user.greet();
+alert(hello);
+*/
+
+/*
+// Constructor Method
+
+function user(name, age, sex) {
+
+    this.name = name;
+    this.age = age;
+    this.sex = sex;
+
+    this.define = function() {
+        this.name = prompt("What is your name.");
+        this.age = prompt("What is your age.");
+        this.sex = prompt("What is your sex.");
+    }
+
+    this.greeting = function() {
+        return `Hello my name is ${this.name} I am a ${this.age} year old ${this.sex}.`;
+    }
+    
+}
+
+
+let person1 = new user("blank", "blank", "blank");
+person1.define();
+let sentence = person1.greeting();
+alert(sentence);
+*/
+
+// The Odin Project - Javascript.info Execises for Objects
+
+/*
+// Map to Names Problem
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [ john, pete, mary ];
+
+// Problem used to demonstrate map method of arrays
+let names = users.map(user => user.name).join(", ");
+
+alert( names ); // John, Pete, Mar
+*/
+
+/*
+// Map to Objects
+
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+let usersMapped = users.map(user => ({ 
+    fullName: user.name + " " + user.surname,
+    id: user.id
+    })
+)
+
+alert( usersMapped[0].id ) // 1
+alert( usersMapped[0].fullName ) // John Smith
+*/
+
+/*
+// Sort Users by age
+
+function sortByAge(arr) {
+    arr.sort((a, b)=>(a.age-b.age));
+}
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let arr = [ pete, john, mary ];
+
+sortByAge(arr);
+
+// now: [john, mary, pete]
+alert(arr[0].name); // John
+alert(arr[1].name); // Mary
+alert(arr[2].name); // Pete
+*/
